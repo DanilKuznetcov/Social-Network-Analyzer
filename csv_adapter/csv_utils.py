ce7b86b70e4e622fbea9151384e424c9b63dd2f7
DanilKuznetcov <dankuziur@gmail.com>
2025-04-26 09:14:02 +0300

    Added csv adapter

---
+++ csv_adapter/csv_utils.py
@@ -0,0 +1,65 @@
+import os
+import csv
+from pathlib import Path
+from typing import Iterator, Dict
+
+
+class CSVPostWriter:
+    def __init__(
+        self,
+        topic: str,
+        start: str,
+        end: str,
+        root_dir: str = "data",
+        max_lines_per_file: int = 5000,
+    ):
+        self.topic = topic
+        self.start = start
+        self.end = end
+        self.max_lines = max_lines_per_file
+
+        self.output_dir = (
+            Path(root_dir)
+            / f"{self.topic}-{self.start.strftime('%Y_%m_%d')}-{self.end.strftime('%Y_%m_%d')}"
+        )
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+
+        self.current_file_index = 1
+        self.current_line_count = 0
+        self.current_file = None
+        self.writer = None
+
+        self.fields_order = [
+            "date",
+            "id",
+            "from_id",
+            "owner_id",
+            "reposts",
+            "likes",
+            "views",
+            "comments",
+            "text",
+        ]
+
+    def _open_new_file(self):
+        if self.current_file:
+            self.current_file.close()
+
+        file_path = self.output_dir / f"posts_part_{self.current_file_index}.csv"
+        self.current_file = open(file_path, mode="w", newline="", encoding="utf-8")
+        self.writer = csv.DictWriter(self.current_file, fieldnames=self.fields_order)
+        self.writer.writeheader()
+        self.current_line_count = 0
+        self.current_file_index += 1
+
+    def write_posts(self, posts: Iterator[Dict]):
+        for post in posts:
+            if self.writer is None or self.current_line_count >= self.max_lines:
+                self._open_new_file()
+            self.writer.writerow({k: post.get(k, "") for k in self.fields_order})
+            self.current_line_count += 1
+
+    def close(self):
+        if self.current_file:
+            self.current_file.close()
+            self.current_file = None
--- main.py
+++ main.py
@@ -2,19 +2,26 @@ from datetime import datetime
 
 from network_agent import VKAgent
 from preprocessing.preprocessing_utils import TextPreprocessor
+from csv_adapter.csv_utils import CSVPostWriter
 
-topic = "COVID-19"
-start = datetime(2024, 8, 11, 11)
-end = datetime(2024, 8, 11, 12)
+
+topic = "высшее образование"
+start = datetime(2024, 2, 1)
+end = datetime(2024, 2, 2)
 
 agent = VKAgent()
 VK_generator = agent.vk_post_generator(topic, start, end)
 
-preprocessor = TextPreprocessor(
-    custom_stopwords_path="preprocessing/all_stop_words.txt"
-)
+print(len(list(VK_generator)))
 
-preprocessor_generator = preprocessor.preprocess_generator(VK_generator)
+# preprocessor = TextPreprocessor(
+#     custom_stopwords_path="preprocessing/all_stop_words.txt"
+# )
+# preprocessor_generator = preprocessor.preprocess_generator(VK_generator)
 
 # print(list(preprocessor_generator))
-print(len(list(preprocessor_generator)))
+# print(len(list(preprocessor_generator)))
+
+# writer = CSVPostWriter(topic, start, end)
+# writer.write_posts(preprocessor_generator)
+# writer.close()
